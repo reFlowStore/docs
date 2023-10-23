09e068debf8729cc585333a2a6b4a57f80f38943
Kevin Boon <kevin_boon@outlook.com>
2023-10-23 01:13:12 -0400

    Update

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://docs.sloppydesigns.com/',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  outDir: './out',
-};
