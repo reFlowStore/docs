9d3515fe69a55da5845c89f713cac72fa0cb22d2
Kallock <56092478+Kallock@users.noreply.github.com>
2025-04-22 03:29:26 -0600

    Finally

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
 import { useRouter } from 'next/router';
-import { StoreIcon, AboutIcon, Unicorn, Pizza, Dialog, Discord, reFlowOS, Hunting, BridgeIcon } from '@components/Icons';
+import { StoreIcon, AboutIcon, Unicorn, Pizza, Dialog, Discord, reFlowOS, Hunting, TrapPhone, BridgeIcon } from '@components/Icons';
 
 function useHead() {
   const { asPath } = useRouter();
